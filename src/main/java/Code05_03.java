2ce2ff78e4709c101e8cca10ae8ac951ba5ef59c
Park-Seung-Gi <parksg1101@naver.com>
2026-03-31 10:25:29 +0900

    26/03/31 수업 if ~ else 문

---
+++ src/main/java/Code05_03.java
@@ -0,0 +1,14 @@
+public class Code05_03 {
+    public static void main(String[] args){
+        int num = 99;
+
+        if (num < 100) {
+            System.out.println("100보다 작습니다.");
+        }
+        else {
+            System.out.println("100보다 큽니다.");
+        }
+
+
+    }
+}
