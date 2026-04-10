977add00d80d39ad3fd415490054f4d8dca370c6
Park-Seung-Gi <parksg1101@naver.com>
2026-04-10 15:00:09 +0900

    26/04/10 중첩 for문

---
+++ src/main/java/Code06_09.java
@@ -0,0 +1,10 @@
+public class Code06_09 {
+    public static void main(String[] args){
+
+        int i,k;
+
+        for (i=0;i<3;i++)
+            for (k=0;k<2;k++)
+                System.out.println("가나다라"+" (i=" + i + " k=" + k + ")");
+    }
+}
