85d500126f4cf261a4ca5fcdb19b3e4e72fc407e
Park-Seung-Gi <parksg1101@naver.com>
2026-04-10 15:02:36 +0900

    26/04/10 While문

---
+++ src/main/java/Code06_10.java
@@ -0,0 +1,10 @@
+public class Code06_10 {
+    public static void main(String[] args){
+        int i = 0;
+
+        while (i<3) {
+            System.out.println(i + "번 반복");
+            i++;
+        }
+    }
+}
