4928b14bd40daad00bbd1ddbe7b2c58baa961512
Park-Seung-Gi <parksg1101@naver.com>
2026-04-17 15:16:53 +0900

    26/04/17 객체

---
+++ src/main/java/Code07_06.java
@@ -0,0 +1,46 @@
+class Rabbit {
+    String shape;
+    int xPos;
+    int yPos;
+
+    Rabbit() {
+        shape = "토끼";
+    }
+
+
+    Rabbit(String value) {
+        shape = value;
+    }
+
+
+    void setPosition(int x, int y ) {
+        xPos = x;
+        yPos = y;
+        System.out.println(x+y);
+    }
+}
+
+
+public class Code07_06 {
+    public static void main(String[] args){
+        Rabbit rabbit1 = new Rabbit("원");
+        Rabbit rabbit2 = new Rabbit("삼각형");
+        Rabbit rabbit3 = new Rabbit("토끼");
+
+        //rabbit1.shape = "원";
+        //rabbit2.shape = "삼각형";
+        //rabbit3.shape = "토끼";
+
+        rabbit1.setPosition(100,100);
+        rabbit2.setPosition(-100,100);
+        rabbit3.setPosition(0,-100);
+
+        Rabbit rabbit = new Rabbit();
+
+        System.out.printf("토끼 객체의 기본 모양은 [%s]입니다. \n",rabbit.shape);
+
+        System.out.printf("토끼1 객체의 모양은 [%s]입니다. \n",rabbit1.shape);
+        System.out.printf("토끼1 객체의 모양은 [%s]입니다. \n",rabbit2.shape);
+        System.out.printf("토끼1 객체의 모양은 [%s]입니다. \n",rabbit3.shape);
+    }
+}
