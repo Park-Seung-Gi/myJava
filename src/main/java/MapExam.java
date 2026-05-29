fd25b253efa25afd1587b067faca716d9755fbba
Park-Seung-Gi <parksg1101@naver.com>
2026-05-29 14:32:04 +0900

    26/05/29

---
+++ src/main/java/MapExam.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExam {
+    public static void main(String[] args){
+
+        Map<String, String> map = new HashMap<>();
+
+        map.put("name","dqwd");
+        map.put("email","fwef@naver.com");
+        map.put("dept","f5fe56");
+
+        System.out.println("조회결과");
+        System.out.println("name : " + map.get("name"));
+        System.out.println("email : " + map.get("email"));
+        System.out.println("dept : " + map.get("dept"));
+
+        map.entrySet().forEach(entry -> {
+           System.out.println(entry.getKey() + " : " + entry.getValue());
+        });
+    }
+}
